97af4b9b83a2f07d831c60fcf5c2f52833d8d871
Steven Steinberg <sstein18@gmail.com>
2015-03-30 20:59:43 -0400

    First Homework Assignment for angularJS

---
+++ index.js
@@ -0,0 +1,15 @@
+angular.module('radioExample', [])
+
+
+    .controller('RadioController', ['$scope', function($scope) {
+      $scope.color = {
+      	var self=this;
+        name: 'white'
+      };
+      //$scope.specialValue = {
+      //  "id": "12345",
+      //  "value": "green"
+      //};
+ 
+
+    }]);
